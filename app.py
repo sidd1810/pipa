e1e667e224d1bce21e3e8fcc1995c95e0ac85b9d
siddharth vaghela <siddhu.vaghela2001@gmail.com>
2025-07-07 22:46:02 +0530

    first commit1

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-kaka
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from CI/CD with Podman!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
